c3ab02a3855c1b012006f09a9ca17ce9232f1096
zhangmj <zhangmj1@dustess.com>
2024-03-26 11:21:23 +0800

    feat(stringx): add file exist func

--- stringx/file.go
+++ stringx/file.go
@@ -3,6 +3,7 @@ package stringx
 import (
 	"bufio"
 	"io"
+	"os"
 )
 
 // ReadLines 逐行读取字符串
@@ -15,4 +16,15 @@ func ReadLines(input io.Reader, cb func(s string)) error {
 		}
 	}
 	return nil
+}
+
+func FileExist(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+		return false
+	}
+	return true
 }
